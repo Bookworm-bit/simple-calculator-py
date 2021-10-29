a8c700b481b83b45a2a70ca578ff02f47989e1bf
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2021-10-29 19:47:26 -0400

    Add files via upload

---
+++ calculatorBasicMain.py
@@ -0,0 +1,16 @@
+o = input("Select your operator! (add = 1, subtract = 2, multiply = 3, divide = 4, power of = 5) \n")
+g = input("Enter your first number! \n")
+h = input("Enter your second number! \n")
+ans = ""
+
+if (int(o) == 1):
+    ans = int(g) + int(h)
+if (int(o) == 2):
+    ans = int(g) - int(h)
+if (int(o) == 3):
+    ans = int(g) * int(h)
+if (int(o) == 4):
+    ans = int(g) / int(h)
+if (int(o) == 5):
+    ans = pow(int(g), int(h))
+print("The answer is", ans, "!")
