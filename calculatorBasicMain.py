27d4d8ac7a6723f9814ab57e916db2065117e046
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2021-11-01 16:04:56 -0400

    Update calculatorBasicMain.py

--- calculatorBasicMain.py
+++ calculatorBasicMain.py
@@ -2,15 +2,15 @@ o = input("Select your operator! (add = 1, subtract = 2, multiply = 3, divide = 
 g = input("Enter your first number! \n")
 h = input("Enter your second number! \n")
 ans = ""
-
-if (int(o) == 1):
-    ans = int(g) + int(h)
-if (int(o) == 2):
-    ans = int(g) - int(h)
-if (int(o) == 3):
-    ans = int(g) * int(h)
-if (int(o) == 4):
-    ans = int(g) / int(h)
-if (int(o) == 5):
-    ans = pow(int(g), int(h))
-print("The answer is", ans, "!")
+whileTrue:
+    if (int(o) == 1):
+        ans = int(g) + int(h)
+    if (int(o) == 2):
+        ans = int(g) - int(h)
+    if (int(o) == 3):
+        ans = int(g) * int(h)
+    if (int(o) == 4):
+        ans = int(g) / int(h)
+    if (int(o) == 5):
+        ans = pow(int(g), int(h))
+    print("The answer is", ans, "!")
