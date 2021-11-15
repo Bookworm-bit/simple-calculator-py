32599ca7f234fc2821df46eff52e911b770388ad
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2021-11-15 12:38:19 -0500

    trys to check if things are numbers

--- calculatorBasicMain.py
+++ calculatorBasicMain.py
@@ -1,16 +1,22 @@
-o = input("Select your operator! (add = 1, subtract = 2, multiply = 3, divide = 4, power of = 5) \n")
-g = input("Enter your first number! \n")
-h = input("Enter your second number! \n")
-ans = ""
-whileTrue:
+import math
+
+while True:
+    o = input("Select your operator! (add = 1, subtract = 2, multiply = 3, divide = 4, power of = 5) \n")
+    g = input("Enter your first number! \n")
+    h = input("Enter your second number! \n")
+    try:
+        int(o)
+    except ValueError:
+        print("Please enter an integer value for your choice of operation!")
+    ans = ""
     if (int(o) == 1):
-        ans = int(g) + int(h)
+        ans = float(g) + float(h)
     if (int(o) == 2):
-        ans = int(g) - int(h)
+        ans = float(g) - float(h)
     if (int(o) == 3):
-        ans = int(g) * int(h)
+        ans = float(g) * float(h)
     if (int(o) == 4):
-        ans = int(g) / int(h)
+            ans = float(g) / float(h)
     if (int(o) == 5):
-        ans = pow(int(g), int(h))
-    print("The answer is", ans, "!")
+        ans = pow(float(g), float(h))
+    print("The answer is", flaot(ans))
